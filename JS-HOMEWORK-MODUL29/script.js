2b624b78e18bb03992589e002a5180204b2e24de
Valeria Romanovska <96697227+ValeriaRomanovskaia@users.noreply.github.com>
2023-12-28 14:38:13 +0200

    Add files via upload

---
+++ JS-HOMEWORK-MODUL29/script.js
@@ -0,0 +1,26 @@
+// ДОМАШНЄ ЗАВДАННЯ МОДУЛЬ 29
+
+// -----1----
+// Напишіть такий JavaScript, щоб після натискання на кнопку button, елемент
+// <div id="text"> зникав.
+
+// const btn = document.querySelector("#btn");
+// const text = document.querySelector("#text");
+
+// btn.addEventListener("click", () => {
+//   document.getElementById("text").hidden = true;
+// });
+
+// -----2----
+// Напишіть такий код, щоб після натискання на кнопку, вона зникала.
+
+// const btn = document.querySelector("#btn");
+
+// btn.addEventListener("click", () => {
+//   document.getElementById("btn").hidden = true;
+// });
+
+// -----3----
+// Створіть дерево, яке показує/приховує дочірні вузли при кліці
+
+// - не зрозуміла як зробити
